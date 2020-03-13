2ef1e19bbf60c07495cc009a2cba09364c9ebd08
Ubuntu <ubuntu@ip-172-31-81-230.ec2.internal>
2020-03-13 18:21:34 +0000

    platformer

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,49 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the createPlatform Function to create platforms for the level. 
+     * 
+     * createPlatform() takes these arguments:
+     *      
+     *      createPlatform(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    function init(game) {
+        let createPlatform = platform.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
+
+        // example:
+        createPlatform(400, 378, 0.8, 0.6);
+        createPlatform(170, 600, 1, 0.6);
+        createPlatform(225, 485, 1, 0.6);
+        createPlatform(280, 265, 1, 0.6);
+        createPlatform(205, 156, 1, 0.6);
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    }
+    platform.init = init;
+})(window);
